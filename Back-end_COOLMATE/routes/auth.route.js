de3e84269ed37a0d1e3decd42682857b9a3f0e39
luonggiotaiii <caodinhluong04@gmail.com>
2025-03-31 10:39:35 +0700

    rm folder

--- Back-end_COOLMATE/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-var router = express.Router();
-const authMiddleware = require('../middleware/auth.middleware');
-const authController = require('../controllers/auth.controller');
-
-router.post('/login', authController.login);
-router.get('/protected', authMiddleware, authController.protectedRoute);
-
-module.exports = router;
--- Front-end_User/src/pages/RegisterPage.jsx
+++
@@ -1,9 +0,0 @@
-const RegisterPage = () => {
-    return(
-        <div>
-            <h1>RegisterPage Page </h1>
-        </div>
-    )
-};
-
-export default RegisterPage;
